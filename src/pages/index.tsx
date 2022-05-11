86c931cd59d81ca4f3cb7c2264f178cf5faa0295
S. Suzuki <terrierscript@gmail.com>
2022-05-11 20:04:52 +0900

    wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
-import { Box, Input, Link } from '@chakra-ui/react'
+import { Box, Input, Link, Stack } from '@chakra-ui/react'
 import Head from 'next/head'
-import React, { useRef, useState } from 'react'
+import React, { useMemo, useRef, useState } from 'react'
 
 export default function Home() {
   const [img, setImg] = useState<string>()
   const canvasRef = useRef<HTMLCanvasElement>()
+  const img2 = useMemo(() => {
+    return img?.split(",")[1]
+  }, [img])
   console.log(img, img?.length)
   return (
     <Box>
@@ -18,23 +21,47 @@ export default function Home() {
           const [file] = e.target.files
           const reader = new FileReader()
           reader.onload = e => {
-            const img = e.target.result.toString().split(",")[1]
-            setImg(img)
-            if (!canvasRef.current) {
-              return
-            }
-            // const ctx = canvasRef.current.getContext('2d')
+            const img = e.target.result
+            // .toString()
+            // .split(",")[1]
+            // setImg(img.toString())
           }
+          if (!canvasRef.current) {
+            return
+          }
+          const imgC = new Image()
+          // canvasRef.current.width = imgC.width
+          // canvasRef.current.height = imgC.height
+          const ctx = canvasRef.current.getContext('2d')
+          console.log(URL.createObjectURL(file))
+          imgC.onload = () => {
+            ctx.drawImage(imgC, 10, 10)
+            setImg(canvasRef.current.toDataURL())
+          }
+          imgC.src = URL.createObjectURL(file)
           reader.readAsDataURL(file)
         }} />
-        <Box>
-          {img && <a target="_blank" href={`/img/${encodeURIComponent(img)}`}>
+        <Stack>
+          <Box>
+            LEN:{img2?.length}
+          </Box>
+          {img && <a target="_blank" href={`/img/${encodeURIComponent(img2)}`}>
             preview
           </a>}
-        </Box>
+          {img && <a target="_blank" href={`/img/${encodeURIComponent(img2).slice(0, 5000000)}`}>
+            preview
+          </a>}
+          {img && <a target="_blank" href={`/img/${encodeURIComponent(img).slice(0, 10000)}`}>
+            preview2
+          </a>}
+          {img && <a target="_blank" href={`/img/${encodeURIComponent(img).slice(0, 100000)}`}>
+            preview2
+          </a>}
+        </Stack>
         <canvas ref={canvasRef} >
 
         </canvas>
+        <Box>xxxx</Box>
       </Box>
     </Box>
   )
