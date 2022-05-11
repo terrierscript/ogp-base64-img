860939d780817382fa4975ad5a7f64c507d98759
S. Suzuki <terrierscript@gmail.com>
2022-05-11 22:43:35 +0900

    meta

--- src/pages/img/[base64].tsx
+++ src/pages/img/[base64].tsx
@@ -1,22 +1,49 @@
 import { Box } from "@chakra-ui/react"
 import { useRouter } from "next/router"
 import { useMemo } from "react"
-
-export const Page = () => {
+import Head from "next/head"
+import { GetStaticPaths, GetStaticProps } from "next"
+export const Page = ({ data }) => {
   const route = useRouter()
-  const base64 = useMemo(() => {
-    const header = "data:image/webp;base64"
-    const revert = route?.query?.base64?.toString()
-      .replaceAll("-", "+")
-      .replaceAll("_", "/")
+  // const base64 = useMemo(() => {
+  //   const header = "data:image/webp;base64"
+  //   const revert = route?.query?.base64?.toString()
+  //     .replaceAll("-", "+")
+  //     .replaceAll("_", "/")
 
-    return `${header},${revert}`
-  }, [route])
+  //   return `${header},${revert}`
+  // }, [route])
   // console.log(r)
 
   return <Box>
-    <img src={base64} />
+    <Head>
+      <title>img</title>
+      <meta property="og:image" content={data} />
+    </Head>
+    <img src={data} />
   </Box>
 }
 
+export const getStaticPaths: GetStaticPaths = async (req) => {
+  return {
+    paths: [],
+    fallback: "blocking"
+  }
+}
+
+export const getStaticProps: GetStaticProps = ({ params }) => {
+  const header = "data:image/webp;base64"
+  const revert = params.base64?.toString()
+    .replaceAll("-", "+")
+    .replaceAll("_", "/")
+  const data = `${header},${revert}`
+
+  return {
+    props: {
+      data
+    }
+  }
+}
+
 export default Page
+
